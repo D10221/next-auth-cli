28de3182b0e4f84b80e7bf658cef5f88f6a8d86c
Mr D <d10221@protonmail.com>
2020-07-31 13:26:47 +0200

    re-export entry

--- next-auth-cli/index.d.ts
+++ next-auth-cli/index.d.ts
@@ -1,21 +1,2 @@
-/**
- *
- * @param {import("./types").Models} models
- * @param {{namingStrategy?: import("typeorm").NamingStrategyInterface, entityPrefix?: string}} [options]
- * @returns {import("typeorm").Table[]}
- */
-export function toTables(models: import("./types").Models, options?: {
-    namingStrategy?: typeorm.NamingStrategyInterface | undefined;
-    entityPrefix?: string | undefined;
-} | undefined): import("typeorm").Table[];
-/**
- * @param {import("typeorm").ConnectionOptions|string} config
- * @param {import("./types").Models|string} [models]
- * @param {(...args:any)=> any} [log]
- * @returns {Promise<void>}
- */
-export default function nextAuthCli(config: import("typeorm").ConnectionOptions | string, models?: string | import("./types").Models | undefined, log?: ((...args: any) => any) | undefined): Promise<void>;
-export function loadConfig([config, models]: [import("typeorm").ConnectionOptions, import("./types").Models]): [import("typeorm").ConnectionOptions, import("./types").Models];
-export function transform([config, models]: [import("typeorm").ConnectionOptions, import("./types").Models]): [import("typeorm").ConnectionOptions, import("./types").Models];
-export function setup(config: import("typeorm").ConnectionOptions | string, models: import("./types").Models | string): Promise<[import("typeorm").ConnectionOptions, import("./types").Models]>;
-import typeorm from "typeorm";
+export * from "./cli/index.js";
+export{ default } from "./cli/index.js";
